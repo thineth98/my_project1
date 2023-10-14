83a16bca3626b49ffd8918bb61608c88f04bf644
206132G <thinethdmt.20@uom.lk>
2023-10-15 03:27:31 +0530

    programm for coverting cm to meters

---
+++ my programm.py
@@ -0,0 +1,8 @@
+# Get the length in centimeters from the user
+centimeters = float(input("Enter a length in centimeters: "))
+
+# Convert centimeters to meters
+meters = centimeters / 100
+
+# Display the result
+print(f"{centimeters} centimeters is equal to {meters} meters")
