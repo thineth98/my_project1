c3ef0fb577f1c4d26c4cd555ae2f450c0a73b3fa
harshana <harshanamaduranga484@gmail.com>
2023-10-15 20:35:32 +0530

    i add new code for convert feet into centimeters

--- my programm.py
+++ my programm.py
@@ -6,3 +6,19 @@ meters = centimeters / 100
 
 # Display the result
 print(f"{centimeters} centimeters is equal to {meters} meters")
+
+
+#new progarame for this
+# Function to convert feet to centimeters
+def feet_to_centimeters(feet):
+    centimeters = feet * 30.48  # 1 foot = 30.48 centimeters
+    return centimeters
+
+# Input from the user
+feet = float(input("Enter the length in feet: "))
+
+# Convert feet to centimeters
+centimeters = feet_to_centimeters(feet)
+
+# Display the result
+print(f"{feet} feet is equal to {centimeters} centimeters")
